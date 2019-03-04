a777b0f630e5aa6e0dfdea543224c0de1f025ca2
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 07:32:46 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,17 @@
 def hello_t(array)
+  if block_given?
   i = 0
+  
 while i < array.length
-  yield array[i]
+  yield (array[i])
   i = i + 1
 end
-array
-end
 
+array
+else 
+  puts "Hey! No block was given!"
+end
+end 
 # call your method here!
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
@@ -14,3 +19,5 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
     puts "Hi, #{name}"
   end
 end
+
+
